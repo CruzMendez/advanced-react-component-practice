94f075181aeece5bd9e5bc0ba24e1392cf80480f
CruzMendez <camendez@utexas.edu>
2017-10-17 19:06:31 -0500

    components/modules created
    left a few out in the interest of time.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Comments from "./components/Comments";
 import Tasks from "./components/Tasks"
 import Orders from "./components/Orders"
 import Tickets from "./components/Tickets"
+import TasksPanel from "./components/TasksPanel"
+import TransactionsPanel from "./components/TransactionsPanel"
 
 function App(props) {
  
@@ -13,13 +15,11 @@ function App(props) {
     <div> 
       <div id="wrapper">
         <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
-          {/*   <TopNav>   */}
+          
           <TopNav />
-          {/*   </TopNav>   */}
-
-          {/*   <SideNav>   */}
+                    
           <SideNav />
-          {/*   </SideNav>   */}
+          
         </nav>
 
         <div id="page-wrapper">
@@ -40,27 +40,15 @@ function App(props) {
                     </div>
                 </div>
                 <div className="row">
-
-                   {/*   </Comments>   */}
+                
                    <Comments/>
-                    {/*   </Comments>   */}
-
-                    {/*   <Tasks>   */}
+                                        
                     <Tasks/>
-                    {/*   </Tasks>   */}
-
-                    {/*   <Orders>   */}
+                                    
                     <Orders/>
-                    {/*   </Orders>   */}
-
-
-                    {/*   <Tickets>   */}
+                                        
                     <Tickets/>
-                    {/*   </Tickets>   */}
-
-
-
-
+                    
                 </div>
                 
                 {/*   <AreaChart>   */}
@@ -80,7 +68,6 @@ function App(props) {
 
                 <div className="row">
 
-
                     {/*   <DonutChart>   */}
                     <div className="col-lg-4">
                         <div className="panel panel-default">
@@ -99,68 +86,14 @@ function App(props) {
 
 
                     <div className="col-lg-4">
-                        {/*   <TasksPanel>   */}
-                        <div className="panel panel-default">
-                            <div className="panel-heading">
-                                <h3 className="panel-title"><i className="fa fa-clock-o fa-fw"></i> Tasks Panel</h3>
-                            </div>
-                            <div className="panel-body">
-                                <div className="list-group">
-                                    {/*   <TaskItem>    */}
-                                    <a href="#" className="list-group-item">
-                                        <span className="badge">just now</span>
-                                        <i className="fa fa-fw fa-calendar"></i> Calendar updated
-                                    </a>
-                                    {/*   </TaskItem>    */}
-                                    
-                                </div>
-                                <div className="text-right">
-                                    <a href="#">View All Activity <i className="fa fa-arrow-circle-right"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        {/*   </TasksPanel>   */}
-
-
+                        
+                        <TasksPanel/>
+                        
                     </div>
                     <div className="col-lg-4">
-
-
-                        {/*   </TransactionsPanel>   */}
-                        <div className="panel panel-default">
-                            <div className="panel-heading">
-                                <h3 className="panel-title"><i className="fa fa-money fa-fw"></i> Transactions Panel</h3>
-                            </div>
-                            <div className="panel-body">
-                                <div className="table-responsive">
-                                    <table className="table table-bordered table-hover table-striped">
-                                        <thead>
-                                            <tr>
-                                                <th>Order #</th>
-                                                <th>Order Date</th>
-                                                <th>Order Time</th>
-                                                <th>Amount (USD)</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {/*   <TransactionRow>   */}
-                                            <tr>
-                                                <td>3326</td>
-                                                <td>10/21/2013</td>
-                                                <td>3:29 PM</td>
-                                                <td>$321.33</td>
-                                            </tr>
-                                            {/*   </TransactionRow>   */}
-                                        </tbody>
-                                    </table>
-                                </div>
-                                <div className="text-right">
-                                    <a href="#">View All Transactions <i className="fa fa-arrow-circle-right"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        {/*   </TransactionsPanel>   */}
-
+                        
+                        <TransactionsPanel/>
+                        
                     </div>
                 </div>
                 
